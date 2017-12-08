8e001a1cf5b65961a9b7f160cefb9751a46c3d39
BIGduzy <nick-b@live.nl>
2017-12-08 16:29:26 +0100

    Project setup

---
+++ Tower-defense/src/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Project name");
+	sf::Event event;
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color(200, 200, 200));
+		window.display();
+	}
+
+    return 0;
+}
