68765901015ac46c0dadd4b09a16a7fc824c92ef
BIGduzy <nick-b@live.nl>
2018-01-17 17:56:18 +0100

    Added a temporary fix for centering text inside a button

--- Tower-defense/src/helpers/MenuItem.cpp
+++ Tower-defense/src/helpers/MenuItem.cpp
@@ -8,15 +8,14 @@ MenuItem::MenuItem(sf::RenderWindow& window, std::function<void()> action, sf::V
 	button(size),
 	text(text)
 {
-	// TODO set text font
 	button.setPosition(position);
 	this->text.setFillColor(sf::Color{0,0,0});
 	// Center text inside the button
 	// TODO this does not work
 	sf::FloatRect textRect = text.getLocalBounds();
 	this->text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-	this->text.setPosition({position.x, position.y}); // We should use this one but it does not work
-	// this->text.setPosition({position.x + size.x / 4, position.y + size.y / 1.5f});
+	// this->text.setPosition({position.x, position.y}); // We should use this one but it does not work
+	this->text.setPosition({position.x + size.x / 4, position.y + size.y / 1.5f});
 }
 
 void MenuItem::onPress() {
