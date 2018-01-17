b54431872896d01dc112a021a1b16c50f6474fdc
Login-is-already-taken <nemkerunkszorolapot@outlook.com>
2018-01-17 12:55:16 +0100

    doxy

--- Tower-defense/src/Enemy/Enemy.h
+++ Tower-defense/src/Enemy/Enemy.h
@@ -55,15 +55,48 @@ public:
 #pragma region comment
 	/// \brief
 	///  lowers hp of enemy
-	/// \param sf::RenderWindow& - Window to be rendered on.
-	/// \param Grid& - Grid, should contain a public std::vector of coordinates of path.
+	/// \param int - damage
+	/// \details
+	/// This function will be called by a projectile object when overlap.
 
 #pragma endregion
 	void decreaseHp(int dmg);
+#pragma region comment
+	/// \brief
+	///  gets position
+
+#pragma endregion
 	sf::Vector2f getPosition();
+#pragma region comment
+	/// \brief
+	///  renders object
+
+#pragma endregion
 	void render();
+#pragma region comment
+	/// \brief
+	///  updates the state of the object
+	/// \details
+	/// This function will be called once per update loop.
+
+#pragma endregion
 	virtual void update() = 0;
+#pragma region comment
+	/// \brief
+	///  returns the amount of gold that the unit gives when killed.
+
+#pragma endregion
 	int getGold();
+#pragma region comment
+	/// \brief
+	///  returns global bounds of object
+
+#pragma endregion
 	sf::FloatRect getBounds();
+#pragma region comment
+	/// \brief
+	///  returns if unit is dead
+
+#pragma endregion
 	bool isDead();
 };
--- Tower-defense/src/Enemy/EnemyAir.h
+++ Tower-defense/src/Enemy/EnemyAir.h
@@ -5,9 +5,24 @@
 class EnemyAir : public Enemy
 {
 public:
+#pragma region comment
+	/// \brief
+	///  constructor
+	/// \param sf::RenderWindow& - Window to be rendered on.
+	/// \param Grid& - Grid, should contain a public std::vector of coordinates of path.
+	/// \param EnemyType - type of unit
+
+#pragma endregion
 	EnemyAir(sf::RenderWindow& window, Grid& grid, EnemyType type);
 	~EnemyAir();
 
+#pragma region comment
+	/// \brief
+	///  updates the state of the object
+	/// \details
+	/// This function will be called once per update loop.
+
+#pragma endregion
 	void update() override;
 };
 
