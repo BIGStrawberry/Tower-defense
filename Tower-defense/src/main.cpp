a290406dadbee44c46d3153ab409a1e6d30bd6ce
Danielvv1998 <daniel.vanvliet@student.hu.nl>
2018-01-17 12:55:25 +0100

    Render enemy

--- Tower-defense/src/main.cpp
+++ Tower-defense/src/main.cpp
@@ -1,11 +1,22 @@
 #include <SFML/Graphics.hpp>
 #include "Tower/Tower.h"
+#include "Enemy/make_enemy.h"
 
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Project name");
 	sf::Event event;
 	Tower t(window, 10.0, sf::Vector2f(500, 200));
+	
+	//fakegrid
+	Grid grid;
+	grid.path.push_back(sf::Vector2f(0, 0));
+	grid.path.push_back(sf::Vector2f(100, 0));
+	grid.path.push_back(sf::Vector2f(100, 100));
+	grid.path.push_back(sf::Vector2f(150, 200));
+
+	std::vector<std::shared_ptr<Enemy>> enemies;
+	enemies.push_back(make_enemy(EnemyType::Normal, window, grid));
 
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
@@ -13,10 +24,17 @@ int main() {
 				window.close();
 			}
 		}
+
+		for (auto& enemy : enemies) {
+			enemy->update();
+		}
 		t.update();
 
 		window.clear(sf::Color(200, 200, 200));
-		//
+
+		for (auto& enemy : enemies) {
+			enemy->render();
+		}
 		t.render();
 		//
 
