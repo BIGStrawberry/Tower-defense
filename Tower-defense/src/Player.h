e68263c261d78ada32bfedf4cf0f1841eb6d6ab4
BIGStrawberry <wouter@highserve.nl>
2018-01-17 13:23:17 +0100

    Player class created

---
+++ Tower-defense/src/Player.h
@@ -0,0 +1,7 @@
+#include "Player.cpp"
+
+Player::Player(sf::RenderWindow & window, uint8_t lives, uint32_t gold, uint32_t score):
+	window(window),
+	lives(lives),
+	gold(gold),
+	score(score) {}
--- Tower-defense/src/main.cpp
+++ Tower-defense/src/main.cpp
@@ -1,9 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include "Player.h"
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Project name");
 	sf::Event event;
-
+	Player player(window, 15, 0, 0);
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
