49daf2535cb2a790a2057b51f800d77c5dd80c70
BIGStrawberry <wouter@highserve.nl>
2018-01-17 14:55:44 +0100

    Whoops - accidentaly added the cpp into the hpp - and vice versa

--- Tower-defense/src/Player.h
+++ Tower-defense/src/Player.h
@@ -1,13 +1,38 @@
-#include "Player.cpp"
+#pragma once
+#include <cstdint>
+#include <SFML/graphics.hpp>
 
 /**
 * @class Player
-* @file Player.h
+* @file Player.cpp
 * @brief Class for the Player
-* @details Keeps track of variables the player has like lives, gold and a score
 */
-Player::Player(sf::RenderWindow & window, uint8_t lives, uint32_t gold, uint32_t score):
-	window(window),
-	lives(lives),
-	gold(gold),
-	score(score) {}
+class Player {
+private:
+	sf::RenderWindow & window;
+
+public:
+	/**
+	* @brief Player Constructor
+	* @param window		SFML window instance
+	* @param lives		amount of lives the player has
+	* @param gold		amount of gold the player has
+	* @param score		amount of score the player has
+	*/
+	Player(sf::RenderWindow & window, uint8_t lives, uint32_t gold, uint32_t score);
+
+	/**
+	* @brief Public variable for the amount of lives the player has
+	*/
+	uint8_t lives;
+
+	/**
+	* @brief Public variable for the amount of gold the player has
+	*/
+	uint32_t gold;
+
+	/**
+	* @brief Public variable for the amount of score the player has
+	*/
+	uint32_t score;
+};
